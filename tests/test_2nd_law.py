9a18510dfffd9cb8b93ebb076f41e24ee34efcd0
Gabriel Tellez <gtellez@SurfacePro8>
2023-03-17 12:08:45 -0500

    Test Jarzynski fuerte que hubiera fallado antes de corregir el bug de redondeo de tiempos

--- tests/test_2nd_law.py
+++ tests/test_2nd_law.py
@@ -56,3 +56,54 @@ def test_Jarzynski_relation(run_sim):
         assert eW == pytest.approx(
             expF[t], abs=tol
         ), f"Jarzynski equality not satisfied at time t={times[t]}"
+
+
+def test_fuerte_Jarzynski_relation(run_sim):
+    """Test the Jarzynski equality
+    <exp(-work)>=exp(-Delta F)
+    for a more variable potential.
+    This used to fail before fixing the rounding bug in the times.
+    """
+
+    ki = 0.5
+    kf = 1.0
+    ko = 20.0
+    tf = 10.0
+    def k(t):
+        """ Protocolo por tramos rectos horizontal """
+        if t<=0.0:
+            return ki
+        if t<=tf/4.0:
+            return ko/2.0
+        if t<tf/2.0:
+            return ko/4.0
+        if t<3.0*tf/4.0:
+            return ko/10.0
+        else:
+            return kf
+        
+    kv = np.vectorize(k)
+
+    simulator = Simulator(
+        tot_sims = 100_000,
+        dt = 0.0001,
+        tot_steps = 10_000,
+        k = k,
+        snapshot_step = 1000,
+    )
+
+    def DeltaF(t):
+        return 0.5 * np.log(kv(t) / k(0))
+    
+    simulator.run()
+    sim = simulator.simulation[0]
+    times = sim.results["times"]
+    work = sim.results["work"]
+    expW = np.average(np.exp(-work), axis=0)
+    expF = np.exp(-DeltaF(times))
+    tol = 5e-2
+    for t, eW in enumerate(expW):
+        assert eW == pytest.approx(
+            expF[t], rel=tol
+        ), f"Jarzynski equality not satisfied at time t={times[t]}"
+
